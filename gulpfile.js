c5610bed7c6e132de68cc95810e5dff302961c3e
Dmitry Denisenko <dmitryesten@gmail.com>
2016-11-27 23:46:16 +0600

    fixed bugs

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
 var gulp  = require('gulp');
+
+var browserSync = require('browser-sync');
+
+var paths = {
+  html:['index.html'],
+  css:['../style/main.scss'],
+  
+};
+
 gulp.task('default', function() {
 	//place code your default task here
 } );
