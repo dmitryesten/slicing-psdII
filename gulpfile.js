ea2193d1a17dffb5907f92dc09a2ddca662e57a0
Dmitry Denisenko <dmitryesten@gmail.com>
2017-01-28 20:37:08 +0600

    made json and change structure gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,42 @@
 var gulp  = require('gulp');
 
+// подключаем gulp-sass
+var sass = require('gulp-sass');
+
+//Синхронизация с сервером
 var browserSync = require('browser-sync');
+
+
+gulp.task('sass', function(){
+	return gulp.src('src/styles/scss/**/*.scss')
+		.pipe(sass()) // используем gulp-sass
+		.pipe(gulp.dest('src/styles'))
+});
+
+gulp.task('browserSync', function() {
+	browserSync({
+		server: {
+				baseDir: 'src'
+				},
+		})
+});
+
+gulp.task('sass', function() {
+	return gulp.src('src/styles/**/*.css')
+		.pipe(sass())
+		.pipe(gulp.dest('src/styles'))
+		.pipe(browserSync.reload({
+		stream: true
+	}))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('src/styles/scss/**/*.scss', ['sass']);
+	// другие ресурсы
+});
+
+
+
 
 var paths = {
   html:['index.html'],
